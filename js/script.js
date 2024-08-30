5612d1058d086e0ad08a6d1d70f9fa0bfde4ecd4
tibcert <info@tibcert.org>
2024-08-30 17:12:59 +0530

    added

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+gsap.from("#logo",{
+    delay:0.6,
+    opacity:0,
+    duration:.6,
+    y:60,
+    scaleY:1.5,
+    scaleX:1.5,
+	}  
+)
+
+
+
+
+
+gsap.to("#logo",{
+    delay:1,
+    opacity:1,
+    duration:1.5,
+    y:20,
+    repeat:-1,
+    yoyo:true
+	}  
+)
+
+
